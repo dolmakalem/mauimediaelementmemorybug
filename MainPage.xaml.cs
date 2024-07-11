0452f4072bf52d11c0d42cb5084463d673f7b32e
dolmakalem <kcomeby@hotmail.com>
2024-07-12 02:28:59 +0300

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Maui.Views;
+using Microsoft.Maui;
+
+namespace MauiApp2
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            player.Source = MediaSource.FromUri(new Uri("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"));
+            player.Play();
+
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
